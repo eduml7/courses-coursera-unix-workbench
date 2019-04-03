1902386faff9f0cb8d2c35d961b9662db79fe81a
Eduardo <egonzalezs@gmail.com>
2019-04-03 16:15:10 +0200

    Coded guess game

---
+++ guessinggame.sh
@@ -0,0 +1,15 @@
+NUM_FILES="$(find . -maxdepth 1 -type f | wc -l)"
+
+function guess_game(){
+  read -p "How many files are in the current directory? insert a number: " user_number_files
+  
+  if [ "$user_number_files" -eq "$NUM_FILES" ]
+  then
+    echo "Yeah"
+    exit 0
+  fi
+}
+
+while true; do
+  guess_game
+done
